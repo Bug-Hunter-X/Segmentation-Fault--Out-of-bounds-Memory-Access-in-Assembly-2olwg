a120ecc39fdb9d8c054bdfd786e98736f866a602
BugHunter-X <askzenai@gmail.com>
2025-02-22 00:20:05 +0100

    Add bug.asm file

---
+++ bug.asm
@@ -0,0 +1,3 @@
+mov eax, [ebx + ecx*4 + 0x10]
+
+This instruction attempts to access memory at an address calculated as [ebx + ecx*4 + 0x10].  If the values in ebx, ecx, and the offset 0x10 result in an address outside of the program's allocated memory space, a segmentation fault will occur. This is a common error when working with arrays or structures where indexing goes out of bounds or pointers are not correctly initialized.
